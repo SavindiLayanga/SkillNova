785ddbfa83851e1afe96694f53996e354696d2ed
Savindi Layanga <savindilayanga@gmail.com>
2026-07-30 22:13:14 +0530

    create cv review page

--- backend/routes/adminCvReviewsRoutes.js
+++ backend/routes/adminCvReviewsRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { verifyAuth, authenticateAdmin } from '../middleware/auth.js';
+import { authenticateAdmin } from '../middleware/auth.js';
 import { getCvReviews } from '../controllers/adminCvReviewsController.js';
 
 const router = express.Router();
 
-router.get('/', verifyAuth, authenticateAdmin, getCvReviews);
+router.get('/', authenticateAdmin, getCvReviews);
 
 export default router;
